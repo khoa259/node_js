a8dc741048d70729f2392b19335670ccde445e08
khoanh <khoa10688@gmail.com>
2022-03-18 15:22:24 +0700

    list products

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,9 @@
 import products from "../models/products"
 
 
-
-export const listProduct = (request,response) => {
+export const listProduct = async (request,response) => {
     try {
-        const product = products.find().exec()
+        const product = await products.find().exec()
         response.json(product)
     } catch (error) {
         response.status(400).json({message:"khong tim thay data"})
@@ -21,13 +20,20 @@ export const createProduct = async (request, response)=>{
         const product = await products(request.body).save();
         response.json(product)
     } catch (error) {
-        response.status(400).json
-        ({message:"khong the tao moi"})
+        response.status(400).json({message:"khong the tao moi"})
     }
 }
 
-export const deleteProduct = (request, response)=>{
-    response.json(products.filter(item => item.id !== +request.params.id));  
+export const deleteProduct = async (request, response)=>{
+    try {
+        const product = await products.findOneAndDelete({_id:request.params.id}).exec()
+        response.json(product)
+    } catch (error) {
+        response.status(400).json({message:"không thể xóa"})
+        
+    }
+    
+    //response.json(products.filter(item => item.id !== +request.params.id));  
 }
 
 export const updateProduct = (request, response)=>{
